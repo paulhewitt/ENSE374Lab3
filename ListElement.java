429bab09b43ece3a298c6300a96185ed26c8ef81
paulhewitt <paulhewitt@Paul-Hewitts-MacBook-Pro.local>
2016-10-05 16:05:31 -0600

    Adding first .java file (test)
    Just testing out git

---
+++ ListElement.java
@@ -0,0 +1,55 @@
+package Lab3;
+
+public class ListElement { // this is a double linked list (hence previous and next) / single list only has next
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data=0;
+		this.next=null;
+		this.previous=null;
+	}
+
+	public void setData(int data)
+	{
+		this.data=data;
+	}
+	
+	public int getData()
+	{
+		return this.data;
+	}
+	
+	public void addElement(ListElement le)
+	{
+		
+	}
+	
+	public ListElement getElement(int index)
+	{
+		
+	}
+	
+	public ListElement deleteElement(int index)
+	{
+		
+	}
+	
+	public void printLinkedListTail()
+	{
+		
+	}
+	
+	public void printLinkedListHead()
+	{
+		
+	}
+	
+	public static void main (String[] args)
+	{
+		
+	}
+	
+}
