15c2334bb59060a3777cf09d2354283260540ed9
paulhewitt <paulhewitt@Paul-Hewitts-MacBook-Pro.local>
2016-10-24 14:38:49 -0600

    Adding the Finished Phase 1 to Git
    Uploading the entire eclipse phase 1 project to the master branch

---
+++ 374Lab3/src/Lab3/LinkedList.java
@@ -0,0 +1,96 @@
+package Lab3;
+
+public class LinkedList {
+	private static ListElement head;
+	
+	public LinkedList()
+	{
+		head=null;
+	}
+	
+	public void addElement(ListElement le)
+	{
+		if (LinkedList.head==null) // if the head of the list is null
+		{
+			head = le;
+		}
+		
+		ListElement node = head;
+		while (node.getNext() != null) // while the next node in the list is not null
+		{
+			node=node.getNext();
+		}
+		node.NextElement(le); // 'setting' the next node
+	}
+	
+	public ListElement getElement (int index)
+	{
+		ListElement node = head;
+		int counter = 0;
+		while (node != null)
+		{
+			if (counter == index)
+			{
+				return node;
+			}
+			counter ++;
+			node = node.getNext();
+		}
+		return new ListElement();
+	}
+	
+	public void deleteElement(int index)
+	{
+		int counter = 0;
+		ListElement node = head;
+		if (node == null) // if the list is empty
+		{
+			return;
+		}
+		while (counter<index) // while the node has not been found
+		{
+			if (node.getNext() != null) // if we are not at the end
+			{
+				counter++; // increment the node
+				if (counter == index) // if we reached the node to delete
+				{
+					node.NextElement(node.getNext().getNext()); // set the previous node to aim at the next node
+					return;
+				}
+				node = node.getNext(); // advance to the next node
+			}
+		}
+		
+	}
+	
+	public void printLinkedListHead()
+	{
+		ListElement node = head;
+		if (node == null) // if list is empty
+		{
+			System.out.println("List is Empty");
+			return;
+		}
+		do{ //other wise print out node data until empty
+			System.out.println(node.getData());
+			node = node.getNext();
+			
+		} while (node.getNext() != null);
+		
+		
+	}
+	
+	public static void main (String [] args){
+		LinkedList mylist = new LinkedList();
+		for (int i=0; i<10; i++)
+		{
+			ListElement le = new ListElement();
+			le.setData(i);
+			mylist.addElement(le);
+		}
+		mylist.printLinkedListHead();
+		mylist.deleteElement(9);
+		mylist.getElement(6).getData();
+	}
+
+}
